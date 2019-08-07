3a54687f8c3dbc5f3383bfffebe2f350da54056c
isabeliroliveira20 <you@example.com>
2019-08-07 16:54:42 -0400

    Adiciona alguma coisa

---
+++ cao-chupando-manga-webapi/src/java/controller/CategoriaController.java
@@ -0,0 +1,14 @@
+
+package controller;
+
+import javax.ws.rs.Path;
+
+   @Path("/categoria")
+public class CategoriaController {
+            
+    @Path("/categoria")
+    public String index(){
+        return "teste";
+    }
+    
+}
